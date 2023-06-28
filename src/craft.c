032fc8ba659d59e2fb81d816c35f10a455a9a569
v0-e <xyz.vieira@gmail.com>
2023-06-28 12:14:24 +0100

    Switch getopt for argp, add optional StationID arg

--- src/craft.c
+++ src/craft.c
@@ -2,12 +2,59 @@
 #include "denm/DENM.h"
 #include <time.h>
 #include <unistd.h>
+#include <argp.h>
+#include <stdbool.h>
+
+const char *argp_program_version = "cits-mc 0.1";
+const char *argp_program_bug_address = "xyz.vieira@gmail.com";
+static char doc[] = "C-ITS random message generator";
+static struct argp_option options[] = { 
+    { "type",         'm', "TYPE",       0,                   "Message type"},
+    { "encoding",     'e', "ENCODING",   0,                   "Message encoding"},
+    { "station-id",   1,   "ID",         OPTION_ARG_OPTIONAL, "Station ID"},
+    { 0 } 
+};
+
+struct fields {
+    int32_t station_id;
+    bool station_id_set;
+};
+
+struct arguments {
+    char* encoding;
+    char* type;
+    struct fields fields;
+};
+
+static error_t parse_opt(int key, char* arg, struct argp_state* state) {
+    struct arguments *arguments = state->input;
+    switch (key) {
+        case 'm': 
+            arguments->type = arg; 
+            break;
+        case 'e': 
+            arguments->encoding = arg; 
+            break;
+        case 1: 
+            arguments->fields.station_id = atoi(arg); 
+            arguments->fields.station_id_set = true; 
+            break;
+        case ARGP_KEY_ARG: 
+            return 0;
+        default: 
+            return ARGP_ERR_UNKNOWN;
+    }   
+    return 0;
+}
+
+static struct argp argp = { options, parse_opt, NULL, doc, 0, 0, 0 };
 
 uint64_t its_ts_get() {
     struct timespec systemtime;
     clock_gettime(CLOCK_REALTIME, &systemtime);
     uint64_t generationdeltatime;
-    generationdeltatime = ((uint64_t)systemtime.tv_sec + 5) * 1000 + (uint64_t)systemtime.tv_nsec / 1E6;
+    generationdeltatime = ((uint64_t)systemtime.tv_sec + 5) * 1000 + 
+        (uint64_t)systemtime.tv_nsec / 1E6;
     return generationdeltatime - 1072915200000;
 }
 
@@ -15,17 +62,23 @@ int64_t rand_range(int64_t min, int64_t max) {
     return (rand() % (1 + max - min)) + min;
 }
 
-CAM_t* gen_cam() {
+CAM_t* gen_cam(struct fields* fields) {
     CAM_t* cam = NULL;
     asn_random_fill(&asn_DEF_CAM, (void**)&cam, 256);
     cam->header.messageID = 2;
     cam->header.protocolVersion = 2;
     cam->header.stationID = rand_range(0, UINT32_MAX);
     cam->cam.generationDeltaTime = its_ts_get() % 65536;
+
+    if (fields) {
+        if (fields->station_id_set) {
+            cam->header.stationID = fields->station_id;
+        }
+    }
     return cam;
 }
 
-DENM_t* gen_denm() {
+DENM_t* gen_denm(struct fields* fields) {
     DENM_t* denm = NULL;
     asn_random_fill(&asn_DEF_DENM, (void**)&denm, 256);
     denm->header.messageID = 1;
@@ -41,9 +94,19 @@ DENM_t* gen_denm() {
             denm->denm.alacarte->stationaryVehicle && 
             denm->denm.alacarte->stationaryVehicle->carryingDangerousGoods &&
             denm->denm.alacarte->stationaryVehicle->carryingDangerousGoods->phoneNumber) {
-        ASN_STRUCT_FREE(asn_DEF_PhoneNumber, denm->denm.alacarte->stationaryVehicle->carryingDangerousGoods->phoneNumber);
+        ASN_STRUCT_FREE(
+                asn_DEF_PhoneNumber, 
+                denm->denm.alacarte->stationaryVehicle->carryingDangerousGoods->phoneNumber
+                );
         denm->denm.alacarte->stationaryVehicle->carryingDangerousGoods->phoneNumber = NULL;
     }
+
+    if (fields) {
+        if (fields->station_id_set) {
+            denm->header.stationID = fields->station_id;
+        }
+    }
+
     return denm;
 }
 
@@ -56,39 +119,39 @@ int encode(void* msg, asn_TYPE_descriptor_t* desc, enum asn_transfer_syntax ats,
     return enc.encoded;
 }
 
-unsigned char* request(char* msg_type, char* encoding_type) {
+unsigned char* request(struct arguments* arguments) {
     unsigned char* out = NULL;
-    printf("encoding: %s | message: %s\n", encoding_type, msg_type);
+    printf("encoding: %s | message: %s\n", arguments->encoding, arguments->type);
 
     void* its_msg;
     asn_TYPE_descriptor_t* desc;
     enum asn_transfer_syntax ats;
-    if (!strcmp(msg_type, "CAM") || !strcmp(msg_type, "cam")) {
+    if (!strcmp(arguments->type, "CAM") || !strcmp(arguments->type, "cam")) {
         desc = &asn_DEF_CAM;
-        its_msg = gen_cam();
-    } else if (!strcmp(msg_type, "DENM") || !strcmp(msg_type, "denm")) {
+        its_msg = gen_cam(&arguments->fields);
+    } else if (!strcmp(arguments->type, "DENM") || !strcmp(arguments->type, "denm")) {
         desc = &asn_DEF_DENM;
-        its_msg = gen_denm();
+        its_msg = gen_denm(&arguments->fields);
     } else {
         return NULL;
     }
 
     int to_hex = 0;
-    if (!strcmp(encoding_type, "oer")) {
+    if (!strcmp(arguments->encoding, "oer")) {
         ats = ATS_CANONICAL_OER;
         to_hex = 1;
-    } else if (!strcmp(encoding_type, "uper")) {
+    } else if (!strcmp(arguments->encoding, "uper")) {
         ats = ATS_UNALIGNED_CANONICAL_PER;
         to_hex = 1;
-    } else if (!strcmp(encoding_type, "ber")) {
+    } else if (!strcmp(arguments->encoding, "ber")) {
         ats = ATS_BER;
         to_hex = 1;
-    } else if (!strcmp(encoding_type, "der")) {
+    } else if (!strcmp(arguments->encoding, "der")) {
         ats = ATS_DER;
         to_hex = 1;
-    } else if (!strcmp(encoding_type, "xml")) {
+    } else if (!strcmp(arguments->encoding, "xml")) {
         ats = ATS_BASIC_XER;
-    } else if (!strcmp(encoding_type, "json")) {
+    } else if (!strcmp(arguments->encoding, "json")) {
         ats = ATS_JER;
     } else {
         return NULL;
@@ -119,26 +182,12 @@ unsigned char* request(char* msg_type, char* encoding_type) {
 
 int main(int argc, char* argv[]) {
     srand(time(NULL));
-    int opt;
-    char* msg_type = NULL;
-    char* encoding_type = NULL;
-    while ((opt = getopt(argc, argv, "e:m:")) != -1) {
-        switch (opt) {
-            case 'm':
-                msg_type = optarg;
-                break;
-            case 'e':
-                encoding_type = optarg;
-                break;
-        }
-    }
-    if (!msg_type || !encoding_type) {
-        fprintf(stderr, "Usage: %s -m msg-type -e encoding-type\n",
-                argv[0]);
-        exit(EXIT_FAILURE);
-    }
 
-    unsigned char* res = request(msg_type, encoding_type);
+    struct arguments arguments = {0};
+
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    unsigned char* res = request(&arguments);
     printf("message:\n%s\n", res);
     free(res);
 
